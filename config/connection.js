d98ab3f9e4dabd62b3d5d684789e554d45a18496
Mary <pocketdru@gmail.com>
2019-02-03 21:17:22 -0700

    heroku added

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "hacktheplanet",
-    database: "todoagain_db"
+    host: "d13xat1hwxt21t45.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "yjtf91gjdkf33buu",
+    password: "oj20wpf5jln89bhr",
+    database: "kl18g7ep7ivh1c3h",
+    port: 3306
   });
 };
 

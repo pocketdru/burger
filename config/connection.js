7dc409c8bedca92aa0a194306be2038a38d7b97b
Mary <pocketdru@gmail.com>
2019-02-05 19:28:07 -0700

    connection to heroku

--- config/connection.js
+++ config/connection.js
@@ -8,18 +8,18 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     user: "root",
     password: "",
-    database: "burger_db",
+    database: "burgers_db",
     port: 3306
   });
 };
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "root",
+//   database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {
